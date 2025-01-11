a034f79f67205b5c24f91ac8f85dbf22946c99d1
Ariel Neumeyer <aneumeyer.ext@vr.com.br>
2025-01-11 12:16:15 -0300

    Add rota de consulta regularidade veiculo

--- src/main/java/com/fiap/parquimetro/repository/RegistroVeiculoRepository.java
+++ src/main/java/com/fiap/parquimetro/repository/RegistroVeiculoRepository.java
@@ -4,7 +4,10 @@ import com.fiap.parquimetro.domain.entity.RegistroVeiculo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RegistroVeiculoRepository extends MongoRepository<RegistroVeiculo, String>, RegistroVeiculoCustomRepository {
 
+    Optional<RegistroVeiculo> findByPlacaVeiculo(String placaVeiculo);
 }
