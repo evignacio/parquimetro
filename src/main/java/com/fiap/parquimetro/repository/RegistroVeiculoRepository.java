d7c11e0fcb354e230289decb248dd9680f5fcc64
Ariel Neumeyer <aneumeyer.ext@vr.com.br>
2025-01-21 13:37:18 -0300

    fix consulta veiculo duplicado

--- src/main/java/com/fiap/parquimetro/repository/RegistroVeiculoRepository.java
+++ src/main/java/com/fiap/parquimetro/repository/RegistroVeiculoRepository.java
@@ -4,10 +4,10 @@ import com.fiap.parquimetro.domain.entity.RegistroVeiculo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface RegistroVeiculoRepository extends MongoRepository<RegistroVeiculo, String>, RegistroVeiculoCustomRepository {
 
-    Optional<RegistroVeiculo> findByPlacaVeiculo(String placaVeiculo);
+    List<RegistroVeiculo> findAllByPlacaVeiculo(String placaVeiculo);
 }
